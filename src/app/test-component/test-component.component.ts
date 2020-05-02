972b83aa515943a7460e3c05a5c3a48019c5c17a
moru <e1028092@student.tuwien.ac.at>
2020-05-02 12:52:37 +0200

    Initial commit.

---
+++ src/app/test-component/test-component.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-component',
+  templateUrl: './test-component.component.html',
+  styleUrls: ['./test-component.component.css'],
+})
+export class TestComponentComponent implements OnInit {
+  items = [
+    { text: 'lorem ipsum', done: false },
+    { text: 'siet mies', done: true },
+    { text: 'dolor', done: true },
+  ];
+  randomNr = 0.0;
+  wipItemText = '';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    setInterval(() => {
+      //this.zone.run(() => { // <-- why isn't this needed? https://medium.com/@MertzAlertz/what-the-hell-is-zone-js-and-why-is-it-in-my-angular-2-6ff28bcf943e
+      this.randomNr = Math.random();
+    }, 1000);
+  }
+
+  addItem(itemText) {
+    this.items.push({ text: itemText, done: false });
+  }
+}
